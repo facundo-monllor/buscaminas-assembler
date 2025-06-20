06f7a29e53a75713de0dccfeaf89348e0c9e5fd0
facundo-monllor <facundomonllor@gmail.com>
2025-06-20 05:16:49 -0300

    vector y generacion de mapa

--- main.asm
+++ main.asm
@@ -1,7 +1,8 @@
 .data
 
 
-PRESENTACION: .asciz "\nBIENVENIDOS AL BUSCAMINAS ARM\nEN ESTE JUEGO TENDRAN QUE DESCUBRIR TODAS LAS CASILLAS QUE NO TENGAN MINAS, SINO...\nBOOOOOOOOOOOOOOOOOM\nPARA ELEGIR LAS CASILLAS INGRESE LA COLUMNA Y L>LONGITUD_PRESENTACION= .-PRESENTACION
+PRESENTACION: .asciz "\nBIENVENIDOS AL BUSCAMINAS ARM\nEN ESTE JUEGO TENDRAN QUE DESCUBRIR TODAS LAS CASILLAS QUE NO TENGAN MINAS, SINO...\nBOOOOOOOOOOOOOOOOOM\nPARA ELEGIR LAS CASILLAS INGRESE LA COLUMNA Y LA FILA CORRESPONDIENTE, SUERTE!!\n\n"   
+LONGITUD_PRESENTACION= .-PRESENTACION
 
 
 PEDIR_NOMBRE: .asciz "INGRESE SU NOMBRE:       (no mas de 20 maracteres)\n"
@@ -20,7 +21,7 @@ TAMANIO_SELECCIONADO: .asciz "   "
 
 
 CANTIDAD_DE_BOMBA: .asciz "    "
-VECTOR_POSICION_DE_BOMBAS: .word
+VECTOR_POSICION_DE_BOMBAS: .space 72         // 72 bytes, para tener espacio para 72 numeros distintos como maximo
 
 
 PEDIR_FILA: .asciz "INGRESE NUMERO DE FILA: \n"
@@ -36,20 +37,36 @@ COLUMNA_SELECCIONADA: .asciz "   "
 semilla: .word 0
 
 
-MAPA_UNO: .ascii "''''''''\n"
-          .ascii "''''''''\n"
-          .ascii "''''''''\n"
-          .ascii "''''''''\n"
-          .ascii "''''''''\n"
-          .ascii "''''''''\n"
-          .ascii "''''''''\n"
-          .ascii "''''''''\n"
-LONGITUD_MAPA_UNO= .-MAPA_UNO
+MAPA_OCHO:.ascii "--------"
+          .ascii "--------"
+          .ascii "--------"
+          .ascii "--------"
+          .ascii "--------"
+          .ascii "--------"
+          .ascii "--------"
+          .ascii "--------"
+LONGITUD_MAPA_OCHO= .-MAPA_OCHO
+
+MAPA_DOCE:.ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+          .ascii "------------"
+LONGITUD_MAPA_DOCE= .-MAPA_DOCE
 
 
 
 .text
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 //r2 la cantidad de caracteres a imprimir
 //r1 la direccion de la cadena
 imprimir:
@@ -62,9 +79,11 @@ push {r0, r7, lr}
 pop {r0, r7, lr}
 bx lr
 .fnend
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 //r2 la cantidad de caracteres que se van a ingresar
 //r1 cadena donde se va a guardar lo igresado
 pedir_por_consola:
@@ -78,8 +97,11 @@ push {r0, r7, lr}
 pop {r0, r7, lr}
 bx lr
 .fnend
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 // r1 = valor del mapa (1 es 8x8 =64, 2 es 12x12 = 144)
 // r2 = valor del nivel (1 si es facil -> 20%, 2 intermedio -> 30% y 3 dificil -> 50%)
 calcular_cantidad_bombas:
@@ -127,24 +149,27 @@ push {r3, r4, r6, r7, lr}
 pop {r3, r4, r6, r7, lr}
 bx lr
 .fnend
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+////////////////////////////////////////////////////////////
 // r1 = valor del mapa (1 es 8x8 =64, 2 es 12x12 = 144)
 // r2 = direccion de memoria del VECTOR_POSICION_DE_BOMBAS
 // r5 la cantidad de bombas
 generar_posiciones_bomba:
 .fnstart    
-push {r4, lr}
+push {r0, r3, r4, r7, r8, lr}
     cmp r1, #0x31
-    beq mapa_ocho       // si el valor del mapa es 1 salta a mapa_ocho y le asigna 64
+    beq mapa_ocho_posiciones       // si el valor del mapa es 1 salta a mapa_ocho y le asigna 64
                         
     mov r3, #144        //sino le asigna 144
-    bal saltear_mapa_ocho
+    bal saltear_mapa_ocho_posiciones 
     
-    mapa_ocho:
+    mapa_ocho_posiciones :
         mov r3, #64     // se le asigna 64
     
-    saltear_mapa_ocho:
+    saltear_mapa_ocho_posiciones:
 
     // en r3 tenemos el largo del mapa -> 64
     // en r5 la cantidad de bombas -> 12
@@ -160,20 +185,34 @@ push {r4, lr}
         mov r7, #0     // entre 0 y 15
         mov r8, #0     // si el mapa es de 64 casillas necesitamos que el segundo random sea maximo 5, si el mapa es de 144 casillas necesitamos que el segundo random sea maximo 10
 
+        generar_random_r7:
         bl generarRandom
+        cmp r0, #0
+        beq generar_random_r7
         mov r7, r0          // esto queda asi
 
+        generar_random_r8:
         bl generarRandom
+        cmp r0, #0
+        beq generar_random_r8
         mov r8, r0
 
+        cmp r8, #15           //si r8 es igual a 15
+        beq restar_10
+
         cmp r8, #10           //si r8 es mayor a 10
-        bge restar_10
+        bge restar_9
 
         cmp r8, #6            //si r8 es mayor a 6
         bge restar_4
+        bal continuar_flujo
 
         restar_10:
         sub r8, #10
+        bal continuar_flujo
+
+        restar_9:
+        sub r8, #9
         bal continuar_flujo
 
         restar_4:
@@ -184,49 +223,63 @@ push {r4, lr}
         mul r0, r7, r8          // multiplicamos los 2 randoms para obtener la posicion de la bomba
 
         cmp r0, #64                 
-        bge random_no_valido        //si el numero final es mayor a la cantidad de posiciones del mapa saltamos
+        bgt ciclo_generar_randoms_ocho        //si el numero final es mayor a la cantidad de posiciones del mapa saltamos
 
-        //verificar que no exista dentro del vector
+        bl verificar_random_ya_creado   //verificar que no exista dentro del vector
+        cmp r10, #1
+        beq ciclo_generar_randoms_ocho     // vuelvo a empezar si el numero random ya existia en el vector
 
         add r4, #1      //sumamos contador de randoms generados
         str r0, [r2]    //agregamos el random generado al vector
-        add r2, #4      //actualizamos la direccion de memoria
+        add r2, #1      //actualizamos la direccion de memoria
         
         bal ciclo_generar_randoms_ocho      // empezar devuelta
-        
-        random_no_valido:
-            //empezamos devuelta
-
-        
-        
-        
-        
-
-        
-
-
-
-
-
-r7=> tengo un valor random          0 a 15
-r8=> tengo otro valor random        0 a 5 si es nivel_chico // 0 a 10 si es nivel_grande
-r4=> contador de bombas generadoas
-
-multiplicar r7 y r8 y dejo en r0 
-
-si estoy en nivel chico me fijo si es mayor o igual a 64
-si estoy en nivel medio me fijo si es mayor o igual a 144
-
-si es mayor a uno de estos valores o este valor ya esta en el vector -> no sumo bomba, no escribo numero en vector, empiezo el random devuelta
-sino => sumo una bomba, escribo el numero en el vector, empiezo el random devuelta
-
 
 salir_ciclo_generar_randoms:
-pop {r4, lr}
+pop {r0, r3, r4, r7, r8, lr}
 bx lr
 .fnend
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// r4 => cantidad de bombas
+// r0 => valor random
+// salida r10 => 1 si ya existe | 0 si no existe
+verificar_random_ya_creado:
+.fnstart
+push {r1, r2, r3, lr}
+    ldr r1, =VECTOR_POSICION_DE_BOMBAS
+    mov r3, #0   // indice
+    mov r10, #0  //si esta en 1 existe el repetido
+
+    ciclo_verificar_random_ya_creado:
+        cmp r10, #1
+        beq salir_verificar_random_ya_creado
+
+        cmp r3, r4               //cuando se completa el bucle
+        beq salir_verificar_random_ya_creado
+        ldrb r2, [r1]
+
+        cmp r2, r0
+        beq random_repetido_en_vector      // si el numero se repite
+
+        bal avanzar_ciclo_repetido_en_vector   // si el no numero se repite paso al proximo numero
+        
+        random_repetido_en_vector:
+        mov r10, #1
+
+        avanzar_ciclo_repetido_en_vector:
+        add r3, #1   // le sumo uno al indice
+        add r1, #1   // le sumo uno a la direccion de memoria, para acceder a la proxima posicion
+        bal ciclo_verificar_random_ya_creado
+      
+salir_verificar_random_ya_creado:
+pop {r1, r2, r3, lr}
+bx lr
+.fnend
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -251,6 +304,37 @@ pop {r1,r2,r7,lr}
 bx lr
 .fnend
 //////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//r1 direccion del vector 
+//r5 cantidad de bombas
+//colocar_bombas_mapa:
+//.fnstart
+//push {lr}
+
+//    mov r3, #0      //indice
+//     ldr r0, =MAPA_OCHO          //direccion del mapa
+
+//     colocar_bomba_ocho:
+//         cmp r3, r5              //termina el bucle
+//         beq fin_ciclo_colocar_bomba
+        
+//         ldr r2, [r1]            //valor del vector
+//         ldr r4, [r0,r1]
+//         str 'x, [r4]            //escribimos el valor de r2 en el mapa
+        
+
+//         fin_ciclo_colocar_bomba:
+// pop {lr}
+// bx lr
+// .fnend
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 
 
@@ -299,11 +383,17 @@ main:
         
         bl calcular_cantidad_bombas
         
-        // ldr r1, =CANTIDAD_DE_BOMBA
+        // ldr r1, =CANTIDAD_DE _BOMBA
         // mov r2, #4
         // bl imprimir
         
         //r5 esta el valor de hexadecimal de las cantidad de bombas que tiene el mapa 
+
+        ldr r0, =TAMANIO_SELECCIONADO
+        ldrb r1, [r0]
+        ldr r2, =VECTOR_POSICION_DE_BOMBAS
+        // mov r5, #12
+        bl generar_posiciones_bomba
 
     
 
